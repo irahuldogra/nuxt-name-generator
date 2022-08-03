0dbfd489b04ba65088d57b2f9b34c6e56a6ade59
Rahul Dogra <rahuldogra1998@gmail.com>
2022-08-03 16:33:42 +0530

    :sparkles: tailwind cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -29,17 +29,19 @@ const selectedNames = ref<string[]>([]);
 </script>
 
 <template>
-  <div class="container max-w-4xl mx-auto font-sans text-center text-black">
-    <h1 class="text-2xl">Name Generator</h1>
-    <p>Choose your options and click the "Find Names" button below</p>
+  <div class="container max-w-4xl mx-auto text-center">
+    <h1 class="text-2xl font-bold text-white">Name Generator</h1>
+    <p class="my-1 font-light text-white">
+      Choose your options and click the "Find Names" button below
+    </p>
     <div
-      class="relative p-3 mx-auto bg-gradient-to-r from-indigo-100 to-purple-300 rounded-3xl"
+      class="relative p-3 mx-2 md:mx-auto bg-gradient-to-r from-indigo-100 to-purple-300 rounded-3xl"
     >
-      <div>
+      <div class="container mx-auto md:my-2">
         <h4 class="p-2 font-semibold text-md">1) Choose a gender</h4>
-        <section>
+        <div class="mb-2 text-sm font-medium text-white">
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-l-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-l-lg"
             :class="
               options.gender === Gender.BOY &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -49,7 +51,7 @@ const selectedNames = ref<string[]>([]);
             Boy
           </button>
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5"
             :class="
               options.gender === Gender.UNISEX &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -59,7 +61,7 @@ const selectedNames = ref<string[]>([]);
             Unisex
           </button>
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-r-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-r-lg"
             :class="
               options.gender === Gender.GIRL &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -68,15 +70,15 @@ const selectedNames = ref<string[]>([]);
           >
             Girl
           </button>
-        </section>
+        </div>
       </div>
-      <div>
+      <div class="container mx-auto md:my-2">
         <h4 class="p-2 font-semibold text-md">
           2) Choose the name's popularity
         </h4>
-        <div>
+        <div class="mb-2 text-sm font-medium text-white">
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-l-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-l-lg"
             :class="
               options.popularity === Popularity.TRENDY &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -86,7 +88,7 @@ const selectedNames = ref<string[]>([]);
             Trendy
           </button>
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-r-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-r-lg"
             :class="
               options.popularity === Popularity.UNIQUE &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -97,11 +99,11 @@ const selectedNames = ref<string[]>([]);
           </button>
         </div>
       </div>
-      <div>
+      <div class="container mx-auto md:my-2">
         <h4 class="p-2 font-semibold text-md">3) Choose name's length</h4>
-        <div>
+        <div class="mb-2 text-sm font-medium text-white">
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-l-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-l-lg"
             :class="
               options.length === Length.LONG &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -111,7 +113,7 @@ const selectedNames = ref<string[]>([]);
             Long
           </button>
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5"
             :class="
               options.length === Length.ALL &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -121,7 +123,7 @@ const selectedNames = ref<string[]>([]);
             All
           </button>
           <button
-            class="text-white bg-purple-700 hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 rounded-r-lg"
+            class="bg-purple-700 hover:bg-purple-800 px-10 md:px-20 py-2.5 rounded-r-lg"
             :class="
               options.length === Length.SHORT &&
               'outline-none ring-4 ring-purple-300 bg-purple-600 mr-1'
@@ -133,15 +135,15 @@ const selectedNames = ref<string[]>([]);
         </div>
       </div>
       <button
-        class="text-white hover:bg-purple-800 font-medium text-sm px-20 py-2.5 mb-2 mt-4 outline-none bg-purple-600 rounded-2xl focus:bg-purple-700 focus:ring-4 focus:ring-purple-300"
+        class="text-white hover:bg-purple-800 font-medium text-sm px-10 md:px-20 py-2.5 mb-2 mt-4 outline-none bg-purple-600 rounded-2xl focus:bg-purple-700 focus:ring-4 focus:ring-purple-300"
         @click="computeSelectedNames"
       >
         Find Names
       </button>
     </div>
-    <div class="flex flex-wrap mt-4 justify-evenly">
+    <div class="flex flex-wrap mt-4 text-white justify-evenly">
       <div
-        class="relative w-[32%] mb-4 mx-1 text-white bg-purple-400 rounded-2xl p-1 py-8"
+        class="relative w-[32%] mb-4 mx-1 bg-purple-400 rounded-2xl p-1 py-8"
         v-for="name in selectedNames"
         :key="name"
       >
